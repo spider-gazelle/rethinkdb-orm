1a4f57d18d6bbdfd5f55407afbd5b6c703a34999
Caspian Baska <caspianbaska@gmail.com>
2019-01-31 14:11:25 +1100

    wip: basic functionality

---
+++ src/rethinkdb-orm/model.cr
@@ -0,0 +1,11 @@
+require "active-model"
+require "crystal-rethinkdb"
+
+module RethinkDb
+
+  abstract class Model < ActiveModel::Model
+    include ActiveModel::Validations
+    include ActiveModel::Dirty
+  end
+
+end
