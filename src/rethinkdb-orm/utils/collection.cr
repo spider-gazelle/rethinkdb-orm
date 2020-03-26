69afec9f9c293dbab410e7bf3eb412fc769bf5ee
Caspian Baska <caspianbaska@gmail.com>
2020-03-26 16:50:11 +1100

    fix(collection): iterate Atom results
    some crystal-rethinkdb stream queries return entire array responses

--- src/rethinkdb-orm/utils/collection.cr
+++ src/rethinkdb-orm/utils/collection.cr
@@ -11,13 +11,37 @@ module RethinkORM
       @iterator = iterator.is_a?(Iterator) ? iterator : iterator.as_a.each
     end
 
+    @atom_response : Iterator(T)? = nil
+
     def next
-      result = wrapped_next
+      result = if (iter = @atom_response)
+                 iter.next
+               else
+                 wrapped_next
+               end
 
       if result == Iterator::Stop::INSTANCE
         stop
+      elsif result.is_a?(T)
+        result
       else
-        T.from_trusted_json result.to_json
+        begin
+          T.from_trusted_json result.as(RethinkDB::QueryResult).to_json
+        rescue e : JSON::MappingError
+          if e.message.try &.includes?("Expected BeginObject but was BeginArray")
+            atom_iterator = result.as(RethinkDB::QueryResult)
+              .as_a
+              .map(&.to_json)
+              .map(&->T.from_trusted_json(String))
+              .each
+
+            @atom_response = atom_iterator
+
+            atom_iterator.next
+          else
+            raise e
+          end
+        end
       end
     end
 
