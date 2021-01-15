46fc9679a23b25a3636da5611c1f97b88a5fe3e2
Caspian Baska <caspianbaska@gmail.com>
2021-01-15 12:55:48 +1100

    test(error): add specs for RethinkORM::Error::DocumentInvalid

---
+++ spec/error_spec.cr
@@ -0,0 +1,19 @@
+require "./spec_helper"
+
+class RethinkORM::Error
+  describe DocumentInvalid do
+    it "#errors" do
+      m = ModelWithValidations.new("")
+      m.valid?.should be_false
+      e = DocumentInvalid.new(m)
+      e.errors.should eq [{field: :name, message: "is required"}, {field: :age, message: "must be greater than 20"}]
+    end
+
+    it "#to_s" do
+      m = ModelWithValidations.new("")
+      m.valid?.should be_false
+      e = DocumentInvalid.new(m)
+      e.to_s.should eq "name is required, age must be greater than 20"
+    end
+  end
+end
