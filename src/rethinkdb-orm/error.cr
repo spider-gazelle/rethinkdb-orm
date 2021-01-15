1b1ee2acaa9ad09f5167c96bf41d510d4fe9be2f
Caspian Baska <caspianbaska@gmail.com>
2021-01-15 12:35:32 +1100

    refactor(persistence): slight improvment to persistence errors
    - Errors are now more fine-grained for `update!` and `save!`
    - `RethinkORM::Error::DocumentInvalid#to_s` now pretty prints validation
    errors
    - `RethinkORM::Error::DocumentInvalid#inspect_errors` deprecated in
    favour of `RethinkORM::Error::DocumentInvalid#errors`

--- src/rethinkdb-orm/error.cr
+++ src/rethinkdb-orm/error.cr
@@ -15,19 +15,33 @@ class RethinkORM::Error < Exception
   end
 
   class DocumentInvalid < Error
-    getter model
+    getter model : RethinkORM::Base
+    getter errors : Array(NamedTuple(field: Symbol, message: String))
 
-    def initialize(@model : RethinkORM::Base, message)
-      super(message)
-    end
-
-    def inspect_errors
-      @model.errors.map do |e|
+    def initialize(@model, message = "Document has invalid fields")
+      @errors = @model.errors.map do |e|
         {
           field:   e.field,
           message: e.message,
         }
       end
+
+      super(message)
+    end
+
+    def to_s(io : IO)
+      remaining = errors.size
+      errors.each do |error|
+        remaining -= 1
+        io << error[:field].to_s
+        io << " " << error[:message]
+        io << ", " unless remaining.zero?
+      end
+    end
+
+    @[Deprecated("Use `errors` instead")]
+    def inspect_errors
+      errors
     end
   end
 
