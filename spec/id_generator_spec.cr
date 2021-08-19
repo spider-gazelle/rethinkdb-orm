77835217862956df7f7f3989227e962c53324f39
Kim Burgess <kim@place.technology>
2021-08-20 00:43:25 +1000

    feat: improve id generator

--- spec/id_generator_spec.cr
+++ spec/id_generator_spec.cr
@@ -1,19 +1,18 @@
-require "base64"
 require "set"
 require "./spec_helper"
 
-describe RethinkORM::IdGenerator, focus: true do
+describe RethinkORM::IdGenerator do
   model = BasicModel.new
 
-  it "concats the table name with a 12 char base65 tail" do
+  it "concats the table name with a 10 char base65 tail" do
     id = RethinkORM::IdGenerator.next(model)
     name, _, tail = id.partition '-'
     name.should eq "basic_model"
-    tail.should match /^[0-9A-Za-z-_~]{12}$/
+    tail.should match /^[0-9A-Za-z-_~]{10}$/
   end
 
   it "generates unique ids within a single fiber" do
-    seq = 10_000
+    seq = 100_000
     ids = Set(String).new(initial_capacity: seq)
     seq.times { ids << RethinkORM::IdGenerator.next(model) }
     ids.size.should eq seq
@@ -22,7 +21,7 @@ describe RethinkORM::IdGenerator, focus: true do
   it "generates unique ids across fibers" do
     ch = Channel(Array(String)).new
     seq = 1000
-    fib = 10
+    fib = 100
 
     fib.times do
       spawn do
--- src/rethinkdb-orm/utils/id_generator.cr
+++ src/rethinkdb-orm/utils/id_generator.cr
@@ -1,15 +1,66 @@
 require "random"
 require "time"
-require "big"
 
+# Generates time-sortable, collision resistant primary keys.
+#
+# Provides suitable performance with local, high-frequency batch insertions and
+# distributed operation will low collision probability. Generated ID's are in
+# the form `<prefix>-<postfix>` where prefix default to the table name and
+# postfix is a lexicographically sortable 10 character unique identifier.
 class RethinkORM::IdGenerator
-  BASE_65 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-             '-', '_', '~']
-  TIME_OFFSET = 1388534400_i64 # Time.utc(2014, 1, 1).to_unix
+  ENCODING = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '-', '_', '~',
+  }
 
-  # Generate a time-sortable and unique (with high probability) primary key
+  # Coverage to 2050 in 30 bits
+  TIME_OFFSET = Time.utc(2014, 1, 1).to_unix
+
+  # 30-bits of entropy
+  RAND_LEN = 30
+  RAND_GEN = seq max: 2_u32**RAND_LEN
+
+  # Provides a pseudo-random sequence of non-repeating numbers.
+  #
+  # Internally this implements a maximal linear feedback shift register via
+  # Xorshift. For non-seeds this provides a cycle length of `2 ** 32 - 1`.
+  private class LSFR
+    include Iterator(UInt32)
+
+    def initialize(@seed : UInt32)
+      @state = @seed
+    end
+
+    def next
+      @state ^= @state << 13
+      @state ^= @state >> 17
+      @state ^= @state << 5
+      if @state == @seed
+        stop
+      else
+        @state
+      end
+    end
+  end
+
+  # Provides a channel with an infinite stream of psuedo-random values up to
+  # *max* size and a garunteed cycle of at least *max* samples.
+  private def self.seq(max = UInt32::MAX, r = Random::DEFAULT)
+    ch = Channel(UInt32).new
+    spawn do
+      loop do
+        mask = Random::Secure.rand max
+        lfsr = LSFR.new(r.rand UInt32)
+        lfsr.select(&.< max).each do |count|
+          ch.send count ^ mask
+        end
+      end
+    end
+    ch
+  end
+
   def self.next(model)
     "#{model.table_name}-#{postfix}"
   end
@@ -19,27 +70,12 @@ class RethinkORM::IdGenerator
   end
 
   def self.postfix
-    time = Time.utc
-    timestamp = (time.to_unix - TIME_OFFSET) * 1_000_000 + time.nanosecond
-
-    # Random tail renders it improbable that there will ever be an ID clash amongst nodes
-    random_tail = (Random.rand(99_999) + 1).to_s.rjust(5, '0')
-    base_encode("#{timestamp}#{random_tail}", BASE_65)
-  end
-
-  # Converts a string of base10 digits to string in arbitrary base
-  def self.base_encode(number, base)
-    base_size = base.size
-
-    number = BigInt.new(number) if number.is_a?(String)
-
-    converted = [] of Char
-    until number.zero?
-      number, digit = number.divmod(base_size)
-      converted.push(base[digit])
+    String.build do |io|
+      time = Time.utc.to_unix - TIME_OFFSET
+      rand = RAND_GEN.receive
+      (time << RAND_LEN | rand).digits(ENCODING.size).reverse_each do |ord|
+        io << ENCODING[ord]
+      end
     end
-
-    converted << base.first if converted.empty?
-    converted.reverse.join
   end
 end
